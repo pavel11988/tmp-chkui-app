f7df5b1a78699abc8e39271ae50c08ff38461989
pavel11988 <kossoua88@gmail.com>
2022-09-24 15:23:35 +0300

    theme image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,22 @@
-import { Box, Container, Image, Text } from '@chakra-ui/react';
+import { Box, Container, Image, Text, useColorMode } from '@chakra-ui/react';
 
 const Hero = () => {
+  const { colorMode } = useColorMode();
   return (
     <Box>
       <Container maxW="container.lg" position="relative">
         <Image
-          src="https://img.freepik.com/premium-photo/milky-way-galaxy-with-stars-space-dust-universe_33900-3.jpg?w=2000"
+          src={
+            colorMode === 'light'
+              ? 'https://celes.club/uploads/posts/2022-04/1651182839_1-celes-club-p-goluboe-nebo-bez-oblakov-priroda-krasivo-f-1.jpg'
+              : 'https://img.freepik.com/premium-photo/milky-way-galaxy-with-stars-space-dust-universe_33900-3.jpg?w=2000'
+          }
           alt="hero_bg"
           objectFit="cover"
           w="100%"
           h="50vh"
         />
+
         <Box
           position="absolute"
           transform="translate(-50%, -50%)"
